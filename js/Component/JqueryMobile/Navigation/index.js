5268bee220f4eeb8439a04a2a4858a031b684e32
Ken <352840792@qq.com>
2018-01-19 10:39:21 +0800

    提交

---
+++ js/Component/JqueryMobile/Navigation/index.js
@@ -0,0 +1,66 @@
+(function() {
+    let Nav = function(options) {
+        let defaults = {
+            Form: {},
+            value: "" //双向数据绑定字段
+        };
+        this.opts = $.extend({}, defaults, options);
+        this.init();
+        return this;
+    };
+    Nav.prototype = {
+        init: function() {
+            var _this = this,
+                opts = this.opts,
+                form = opts.Form;
+            _this.createComponent();
+            _this.Events();
+            //调用自定义事件
+            if (opts.customEvent) {
+                opts.customEvent.oninitialize ? opts.customEvent.oninitialize(_this.input, _this) : '';
+            }
+        },
+        Events: function() {
+            var _this = this,
+                opts = this.opts;
+            Object.defineProperty(opts, 'des', {
+                get: function() { //获取数据
+                    return this.value;
+                },
+                set: function(val) { //设置值
+                    _this.input.val(val);
+                    this.value = val;
+                }
+            });
+            _this.input.on('change', function() {
+                opts.des = this.value;
+            })
+        },
+        //创建Component表
+        createComponent: function() {
+            var _this = this,
+                opts = this.opts,
+                form = opts.Form;
+            form.content = $('<div class = "ui-field-contain"></div>');
+            form.lable = $('<label for="fname">' + opts.lable + '</label>');
+            form.content.append(form.lable);
+            //必选lable上给星号
+            if (opts.regexp && opts.regexp.require) form.lable.append($('<i style ="color: red"> * </i>'));
+            _this.input = $(opts.input)
+            _this.input.attr('placeholder', opts.placeholder);
+            form.content.append(_this.input);
+            opts._selfFrom.append(form.content);
+            _this.input.textinput();
+        },
+        getValue: function() {
+            return this.opts.value;
+        },
+        setValue: function(arr) {
+            this.opts.des = arr;
+        }
+    };
+    window.Nav = Nav;
+})();
+export {
+    Nav
+}
